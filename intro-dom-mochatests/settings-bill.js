1ff5f740305d7e76acb7789ee3cccae85cb00591
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-04-16 22:19:47 +0200

    third commit

--- intro-dom-mochatests/settings-bill.js
+++ intro-dom-mochatests/settings-bill.js
@@ -1,4 +1,4 @@
-function BillWithSettings() {
+/* function BillWithSettings() {
     var theCallCost = 0;
     var theSmsCost = 0;
     var theWarningLevel = 0;
@@ -89,5 +89,57 @@ function BillWithSettings() {
         getTotalSmsCost,
         sendSms,
         totalClassName
+    }
+} */
+function GreetingTheUSer() {
+    var thenamesGreeted = {};
+    var thegreetingsCounter = 0;
+    var theuserName = " ";
+    var thelanguageType;
+
+    function setName(userName) {
+        theuserName = userName;
+        if (thenamesGreeted[theuserName] === undefined) {
+            thegreetingsCounter++;
+            //add an entry for the user that was greeted in the Object Map
+            thenamesGreeted[userName] = 0;
+            //update the DOM to display the counter
+        }
+    } 
+    function setLanguage(language) {
+        thelanguageType = language;
+    }
+    function getName() {
+        return theuserName;
+    }
+    function getLanguage() {
+        return thelanguageType;
+    }
+
+    function greetingCounter()
+    {
+        return thegreetingsCounter;
+    }
+
+    function greetUser(){
+
+        if(thelanguageType == "english" )
+        {
+            return "Hello, " + getName(); 
+        }
+        else if(thelanguageType == "afrikaans"){
+            return "Hallo, " + getName(); 
+        }
+        else if(thelanguageType == "isiXhosa"){
+            return "Mholweni, " + getName(); 
+        }
+    }
+    return {
+        setName,
+        setLanguage,
+        getName,
+        getLanguage,
+        greetingCounter,
+        greetUser
     }
 }
--- intro-dom-mochatests/test/settings-bill-test.js
+++ intro-dom-mochatests/test/settings-bill-test.js
@@ -1,4 +1,4 @@
-describe("The factory function", function(){
+/* describe("The factory function", function(){
     it("should be able to set the call cost", function(){
         let settingsBill = BillWithSettings();
 
@@ -205,5 +205,121 @@ describe("warning and crital level", function(){
         settingsBill.makeCall();
         settingsBill.makeCall();
         assert(15,settingsBill.getTotalCallCost());
+    })
+}); */
+describe("The bill with settings factory function", function(){
+    it("should be able to set the name", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        assert.equal("Thabo",greeting.getName());
+
+        greeting.setName("Teboho");
+        assert.equal("Teboho",greeting.getName());
+    });
+
+    it("should be able to set the language ", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setLanguage("english");
+        assert.equal("english",greeting.getLanguage());
+
+        greeting.setLanguage("afrikaans");
+        assert.equal("afrikaans",greeting.getLanguage());
+
+        greeting.setLanguage("isiXhosa");
+        assert.equal("isiXhosa",greeting.getLanguage());
+    });
+   
+});
+
+describe("use values", function(){
+    it("should be able to use the name", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        assert.equal(1, greeting.greetingCounter());
+    });
+    it("should be able to greet a person and counter should be 1", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        assert.equal(1, greeting.greetingCounter());
+    });
+
+    it("should be able to greet the same person 3 times and counter should remain 1", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        greeting.setName("Thabo");
+        greeting.setName("Thabo");
+        assert.equal(1, greeting.greetingCounter());
+    });
+
+    it("should be able to greet 3 diferent people and conter should be 3 ", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        greeting.setName("Teboho");
+        greeting.setName("Lerato");
+        assert.equal(3, greeting.greetingCounter());
+
+    });
+
+    it("should be able to greet 2 different people 4 times and counter should be 2", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        greeting.setName("Teboho");
+        greeting.setName("Thabo");
+        greeting.setName("Teboho");
+        greeting.setName("Thabo");
+        greeting.setName("Teboho");
+        assert.equal(2, greeting.greetingCounter());
+
+    });
+    it("should be able to remain 0 if no name is enterd", function(){
+        let greeting = GreetingTheUSer();
+        assert.equal(0, greeting.greetingCounter());
+
+    });
+});
+describe("Displaying greeting message", function(){
+    it("it should display greeting message according to selected language", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Thabo");
+        greeting.setLanguage("english")
+        assert.equal("Hello, Thabo", greeting.greetUser());
+        greeting.setName("Thabo");
+        greeting.setLanguage("afrikaans")
+        assert.equal("Hallo, Thabo", greeting.greetUser());
+        greeting.setName("Thabo");
+        greeting.setLanguage("isiXhosa")
+        assert.equal( "Mholweni, Thabo", greeting.greetUser());
+    })
+    it("it should display greeting message for Teboho in English", function(){
+        let greeting = GreetingTheUSer();
+
+        greeting.setName("Teboho");
+        greeting.setLanguage("english")
+        assert.equal("Hello, Teboho", greeting.greetUser());
+
+    })
+
+    it("it should display greeting message for John in Afrikaans", function(){
+        let greeting = GreetingTheUSer();
+        greeting.setName("John");
+        greeting.setLanguage("afrikaans")
+        assert.equal("Hallo, John", greeting.greetUser());
+
+    })
+
+    it("it should display greeting message for Xoli in isiXhosa", function(){
+        let greeting = GreetingTheUSer();
+        greeting.setName("Xoli");
+        greeting.setLanguage("isiXhosa")
+        assert.equal("Mholweni, Xoli", greeting.greetUser());
+
     })
 });
